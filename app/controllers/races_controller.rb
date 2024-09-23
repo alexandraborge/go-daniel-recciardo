88558aaaf420b702f3bfcd8433980a80cf508f3e
Alexandra Borge <alexandraroseborge@gmail.com>
2024-09-22 20:08:52 -0700

    replace api call with query for all races

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,7 +1,5 @@
 class RacesController < ApplicationController
   def index
-    # I want to save important details from this response to the DB
-    # It will be used to create the association with the drivers
-    race_data = HTTParty.get("https://api.openf1.org/v1/meetings?year=2024")
+    Race.all
   end
 end
