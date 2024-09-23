eb9ab43c3d0fa7788c1da96ba26507e3d6486e92
Alexandra Borge <alexandraroseborge@gmail.com>
2024-09-22 19:39:58 -0700

    update endpoint url name used in tests to match routes more accurately

--- spec/requests/races_spec.rb
+++ spec/requests/races_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Races", type: :request do
-  describe "GET /index" do
+  describe "GET /races" do
     it "returns http success" do
-      get "/races/index"
+      get "/races"
       expect(response).to have_http_status(:success)
     end
 
     it 'gets race information from the OpenF1 API for a given season' do
       expect(HTTParty).to receive(:get).with("https://api.openf1.org/v1/meetings?year=2024")
-      get "/races/index"
+      get "/races"
     end
   end
 end
